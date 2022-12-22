27e786bc778cf7ab095bae51af7927d3b439d6b2
yuki-o1o5 <jkksst1712@gmail.com>
2022-12-22 02:06:32 -0800

    feat/fetch comment in main/[id].jsx and all/[id].jsx

--- pages/main/[id].jsx
+++ pages/main/[id].jsx
@@ -6,15 +6,75 @@ import DetailArticleText from "../../components/DetailArticleText/DetailArticleT
 import DetailArticleTitle from "../../components/DetailArticleTitle/DetailArticleTitle.jsx";
 import PageTitle from "../../components/PageTitle/PageTitle.jsx";
 
-const DetailPage = () => {
+export async function getStaticProps(context) {
+  // 1.This is an id. ->[33935566]
+  const storyId = context.params.id;
+  // console.log("params", context.params);
+
+  // 2.This is a story detail. ->{...}
+  const getDetailUrl = async (id) => {
+    const res = await fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+    );
+    const eachStoryDetails = await res.json();
+    return eachStoryDetails;
+  };
+  const story = await getDetailUrl(storyId);
+
+  // 3.This is the top comment.
+  const getCommentUrl = async (commentId) => {
+    const res = await fetch(
+      `https://hacker-news.firebaseio.com/v0/item/${commentId}.json?print=pretty`
+    );
+    const comments = await res.json();
+    return comments;
+  };
+
+  // const topComment = story.kids ? await getCommentUrl(story.kids[0]) : "";
+  const topComment = story.kids ? await getCommentUrl(story.kids[0]) : "";
+
+  // console.log("topComment", topComment);
+
+  // 3.This is the comments of the top comment .
+  let topCommentReplies = [];
+  if (topComment.kids) {
+    topCommentReplies = await Promise.all(
+      topComment.kids.map((topCommentKid) => getCommentUrl(topCommentKid))
+    );
+  }
+  console.log("topCommentReplies", topCommentReplies);
+
+  return {
+    props: { story, topComment, topCommentReplies },
+    revalidate: 10,
+  };
+}
+
+export async function getStaticPaths() {
+  // 1.This is top 3 story ids.
+  const resOne = await fetch(
+    `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&limitToFirst=3&orderBy="$key"`
+  );
+  const topstories = await resOne.json();
+
+  const paths = topstories.map((topstory) => ({
+    params: { id: topstory.toString() },
+  }));
+  console.log(paths);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const DetailPage = ({ story, topComment, topCommentReplies }) => {
+  // console.log(story);
   return (
     <div>
       <PageTitle />
       <div className={"main_container"}>
         <div className="detail_article_title_container">
-          <DetailArticleTitle
-            detailArticleTitle={"faucibus ornare suspendisse sednisi lacus sed"}
-          />
+          <DetailArticleTitle detailArticleTitle={story.title} />
         </div>
         <div className="article_text_container">
           <DetailArticleCategoryTitle
@@ -34,15 +94,14 @@ const DetailPage = () => {
           />
           <div className="secondry_text-container">
             <DetailArticleCommentParent
-              detailArticleCommentParent={
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              }
+              detailArticleCommentParent={topComment.text}
             />
-            <DetailArticleCommentChild
-              detailArticleCommentChild={
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              }
-            />
+            {topCommentReplies.map((topCommentReply, i) => (
+              <DetailArticleCommentChild
+                detailArticleCommentChild={topCommentReply.text}
+                key={`story-list-${i}`}
+              />
+            ))}
           </div>
         </div>
       </div>
