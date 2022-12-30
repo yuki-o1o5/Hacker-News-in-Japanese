5e982c47bcc8eaf27b01f9d08b4e3c4e9e3a65e9
yuki-o1o5 <jkksst1712@gmail.com>
2022-12-30 15:05:58 -0800

    refactor/use helpers and rename part2

--- pages/main/index.jsx
+++ pages/main/index.jsx
@@ -1,11 +1,12 @@
-import * as deepl from "deepl-node";
 import Article from "../../components/Article/Article.jsx";
 import ArticlesCategoryTitle from "../../components/ArticlesCategoryTitle/ArticlesCategoryTitle.jsx";
 import PageDescription from "../../components/PageDescription/PageDescription.jsx";
 import PageTitle from "../../components/PageTitle/PageTitle.jsx";
+import { getStoryDetail } from "../../helpers/hackerNews/storyDetail";
+import { translateStoryDetail } from "../../helpers/deepl/translateStoryDetail";
+import { JA } from "../../helpers/deepl/common";
 
 export async function getStaticProps() {
-  // 1.This is top 3 story ids.
   let topStoriesIds = [];
   try {
     const getTopStoriesIdsRes = await fetch(
@@ -27,59 +28,13 @@ export async function getStaticProps() {
     };
   }
 
-  // 2.This is each story details.
-  const getTopStoriesUrl = async (id) => {
-    let topStories = [];
-    try {
-      const getTopStoriesRes = await fetch(
-        `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
-      );
-
-      if (!getTopStoriesRes.ok) {
-        return {
-          notFound: true,
-          revalidate: 10,
-        };
-      }
-
-      topStories = await getTopStoriesRes.json();
-      return topStories;
-    } catch (error) {
-      return {
-        notFound: true,
-        revalidate: 10,
-      };
-    }
-  };
   const topStoriesDetails = await Promise.all(
-    topStoriesIds.map((topStoriesId) => getTopStoriesUrl(topStoriesId))
+    topStoriesIds.map((topStoryId) => getStoryDetail(topStoryId))
   );
 
-  //  3.This is each Japanese story details
-  const translateToJapanese = async (text) => {
-    const translator = new deepl.Translator(process.env.DEEPL_AUTH_KEY);
-    const translatedResponse = await translator.translateText(
-      text.title,
-      null,
-      "ja"
-    );
-    // console.log(translatedResponse.text);
-    return {
-      by: text.by || "",
-      descendants: text.descendants || 0,
-      id: text.id || 0,
-      kids: text.kids || [],
-      score: text.score || 0,
-      time: text.time || 0,
-      title: translatedResponse.text || 0,
-      type: text.type || "",
-      url: text.url || "",
-    };
-  };
-
   const japaneseTopStories = await Promise.all(
-    topStoriesDetails.map((topStoriesDetail) =>
-      translateToJapanese(topStoriesDetail)
+    topStoriesDetails.map((topStoryDetail) =>
+      translateStoryDetail(topStoryDetail, JA)
     )
   );
 
@@ -94,7 +49,7 @@ const Mainpage = (props) => {
     <div>
       <PageTitle />
       <div className={"main_container"}>
-        <ArticlesCategoryTitle articlesCategoryTitle={"Recent in Hour"} />
+        <ArticlesCategoryTitle articlesCategoryTitle={"Recent Top 3"} />
 
         {props.japaneseTopStories.map((japaneseTopStory, i) => (
           <Article
@@ -107,7 +62,6 @@ const Mainpage = (props) => {
             articlePoints={japaneseTopStory.score}
           />
         ))}
-
         <PageDescription />
       </div>
     </div>
