d4ea9369ee5cb4ee8fd29e1809769635b68b2236
yuki-o1o5 <jkksst1712@gmail.com>
2023-07-04 22:36:05 -0700

    docs: remove unnecessary components

--- components/ArticleNumber/ArticleNumber.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./ArticleNumber.module.css";
-
-const ArticleNumber = ({ articleNumber }) => {
-  return <i className={styles.articleNumber}>{articleNumber}</i>;
-};
-
-export default ArticleNumber;
--- components/ArticleTitle/ArticleTitle.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./ArticleTitle.module.css";
-
-const ArticleTitle = ({ articleTitle }) => {
-  return <div className={styles.articleTitle}>{articleTitle}</div>;
-};
-
-export default ArticleTitle;
